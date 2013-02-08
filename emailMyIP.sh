2ea944ae647a2a9c7cf86195e6002639341cd882
c4tler <goomfy@gmail.com>
2013-02-08 11:27:04 +1100

    Update emailMyIP.sh
    whatismyip.com changed the way their api works so I switched to a new service

--- emailMyIP.sh
+++ emailMyIP.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP="`curl -s http://automation.whatismyip.com/n09230945.asp 2>&1 | cat`"
+IP="`curl -s http://icanhazip.com 2>&1 | cat`"
 
 SUBJECT="Your IP is:"
 echo $IP | mailx -s "$SUBJECT" you@gmail.com
